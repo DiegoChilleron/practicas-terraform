2d1b4c17e5b753070af48fcd12dd047d92e71201
Diego Chilleron <diegochmu@gmail.com>
2024-08-11 19:21:45 +0200

    práctica 5

---
+++ PRACTICA_5/providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">=5.57.0, !=5.59.0, <5.61.0"
+    }
+  }
+  required_version = ">=1.9.4"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias  = "ohio"
+}
