ab9bafa478d46ba4dbf5f36aacf4a01a95267783
Diego Chilleron <diegochmu@gmail.com>
2024-08-13 12:28:44 +0200

    add Lifecycles - practica 6

--- PRACTICA_6/ec2.tf
+++ PRACTICA_6/ec2.tf
@@ -1,6 +1,15 @@
 resource "aws_instance" "public_instance" {
   ami           = "ami-0ae8f15ae66fe8cda"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id     = aws_subnet.private_subnet.id
   key_name      = data.aws_key_pair.key.key_name
+
+  // lifecycle {
+  //  create_before_destroy = true
+  //  prevent_destroy       = true
+  //  ignore_changes = {}
+  //  replace_triggered_by = []
+  //    aws_subnet.private_subnet.id ]
+  //  }
+
 }
